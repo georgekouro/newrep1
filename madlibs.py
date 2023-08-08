b3454e3dee9a0b246f08bbccfe90bfee24dca359
georgekouro <georgekouro1@gmail.com>
2023-08-08 18:58:10 +0400

    first_work

---
+++ madlibs.py
@@ -0,0 +1,34 @@
+person = input('Input Person ')
+noun = input('Input noun ')
+
+Number = input('Input Number ')
+
+Measure_of_time = input('Input Measure_of_time ')
+
+Mode_of_Transportation = input('Input Mode_of_Transportation ')
+Adjective = input('Input Adjective ')
+Adjective2 = input('Input Adjective2 ')
+Color = input('Input Color ')
+Part_of_the_Body = input('Input Part_of_the_Body ')
+Verb = input('Input Verb ')
+Number2 = input('Input Number2 ')
+
+Noun2 = input('Input Noun2 ')
+
+Noun3 = input('Input Noun3 ')
+
+Part_of_the_Body_2 = input('Input Part_of_the_Body_2 ')
+
+Verb1 = input('Input Verb1 ')
+
+Noun4 = input('Input Noun4 ')
+
+Adjective3 = input('Input Adjective3 ')
+
+Silly_Word = input('Input Silly_Word ')
+
+Noun = input('Input Noun ')
+
+
+
+print(f"It was about {Number} {Measure_of_time} ago when I arrived at the hospital in a {Mode_of_Transportation}. The hospital is a/an {Adjective} place, there are a lot of {Adjective2} {Noun} here. There are nurses here who have {Color} {Part_of_the_Body}. If someone wants to come into my room I told them that they have to {Verb} first. I’ve decorated my room with {Number2} {Noun2}. Today I talked to a doctor and they were wearing a {Noun3} on their {Part_of_the_Body_2}. I heard that all doctors {Verb1} {Noun4} every day for breakfast. The most {Adjective3} thing about being in the hospital is the {Silly_Word} {Noun}")
